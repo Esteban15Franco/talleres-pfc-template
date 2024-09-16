11a44073e33fb06b058e89acf27bab592802e130
Juan_E <juan.e.franco@correounivalle.edu.co>
2024-09-16 11:25:18 -0500

    Ultima correccion, taller completo

--- app/src/main/scala/taller/TorresHanoi.scala
+++ app/src/main/scala/taller/TorresHanoi.scala
@@ -1,28 +1,23 @@
 package taller
 
 class TorresHanoi {
-<<<<<<< HEAD
 
-  def movsTorresHanoi(n: Int): BigInt = {
-=======
-    
+    // Calcula el número de movimientos necesarios para resolver las Torres de Hanoi
     def movsTorresHanoi(n: Int): BigInt = {
->>>>>>> master
-        if(n ==1) 1
-        else 2 * movsTorresHanoi(n-1) + 1
+        if (n == 1) 1
+        else 2 * movsTorresHanoi(n - 1) + 1
     }
 
-    def torresHanoi(n: Int, t1: Int, t2: Int, t3: Int) : List[(Int, Int)] = {
-
-        //caso base
-        if(n == 1){
+    // Devuelve una lista de movimientos necesarios para resolver las Torres de Hanoi
+    def torresHanoi(n: Int, t1: Int, t2: Int, t3: Int): List[(Int, Int)] = {
+        // Caso base
+        if (n == 1) {
             List((t1, t3))
-        }else{
-            val listamovInicial: List[(Int,Int)] = torresHanoi(n-1,t1,t3,t2)
-            val listamovIntermedio: List[(Int, Int)] = listamovInicial:::List((t1,t3))
-            val listamovFinal: List[(Int, Int)] = listamovIntermedio:::torresHanoi(n-1,t2,t1,t3)
+        } else {
+            val listamovInicial: List[(Int, Int)] = torresHanoi(n - 1, t1, t3, t2)
+            val listamovIntermedio: List[(Int, Int)] = listamovInicial ::: List((t1, t3))
+            val listamovFinal: List[(Int, Int)] = listamovIntermedio ::: torresHanoi(n - 1, t2, t1, t3)
             listamovFinal
         }
     }
-
 }
--- app/src/test/scala/taller/MaxlistTest.scala
+++ app/src/test/scala/taller/MaxlistTest.scala
@@ -25,6 +25,22 @@ class MaxlistTest extends AnyFunSuite {
     assert(obj.maxIt(List(28, 11, 9, 5, 20, 15, 21, 12, 0, 1)) == 28)
   }
 
+  test("MaxIt test 4 - Lista con un solo elemento") {
+    assert(obj.maxIt(List(42)) == 42)
+  }
+
+  test("MaxIt test 5 - Lista con elementos negativos") {
+    assert(obj.maxIt(List(-10, -20, -30, -40, -50)) == -10)
+  }
+
+  test("MaxIt test 6 - Lista con elementos negativos y positivos") {
+    assert(obj.maxIt(List(-10, 5, 20, -15, 30, -5)) == 30)
+  }
+
+  test("MaxIt test 7 - Lista con elementos duplicados") {
+    assert(obj.maxIt(List(7, 7, 7, 7, 7, 7)) == 7)
+  }
+
   test("MaxLin test 1 - Lista con varios elementos") {
     assert(obj.maxLin(List(1, 4, 21, 16, 33, 2, 6, 13, 9)) == 33)
   }
@@ -32,6 +48,22 @@ class MaxlistTest extends AnyFunSuite {
   test("MaxLin test 2 - Lista con varios elementos 2")
   {
     assert(obj.maxLin(List(28, 11, 9, 5, 20, 15, 21, 12, 0, 1)) == 28)
+  }
+
+  test("MaxLin test 3 - Lista con un solo elemento") {
+    assert(obj.maxLin(List(42)) == 42)
+  }
+
+  test("MaxLin test 4 - Lista con elementos negativos") {
+    assert(obj.maxLin(List(-10, -20, -30, -40, -50)) == -10)
+  }
+
+  test("MaxLin test 5 - Lista con elementos negativos y positivos") {
+    assert(obj.maxLin(List(-10, 5, 20, -15, 30, -5)) == 30)
+  }
+
+  test("MaxLin test 6 - Lista con elementos duplicados") {
+    assert(obj.maxLin(List(7, 7, 7, 7, 7, 7)) == 7)
   }
 
 }
