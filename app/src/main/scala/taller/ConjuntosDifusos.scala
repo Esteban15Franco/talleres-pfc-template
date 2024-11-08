acd2a31e82594fe848bcf91c5ca4a4ec5f6be8a3
Juan_E <juan.e.franco@correounivalle.edu.co>
2024-11-08 18:28:15 -0500

    funciones union e interseccion añadidas y test listos

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -17,4 +17,20 @@ class ConjuntosDifusos {
   def igualdad(cd1: ConjDifuso, cd2: ConjDifuso, rango: Range): Boolean = {
     rango.forall(elem => pertenece(elem, cd1) == pertenece(elem, cd2))
   }
+
+  def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = {
+    (x: Int) => Math.max(cd1(x), cd2(x))
+  }
+
+  def intersect(cd1: ConjDifuso, cd2: ConjDifuso, rango: Range): ConjDifuso = {
+    (x: Int) => {
+      if (rango.contains(x)) math.min(cd1(x), cd2(x))
+      else 0.0  // O cualquier valor que signifique que el elemento no pertenece
+    }
+  }
+
+
+
+
+
 }
--- app/src/test/scala/taller/ConjuntosDifusosTest.scala
+++ app/src/test/scala/taller/ConjuntosDifusosTest.scala
@@ -11,27 +11,65 @@ class ConjuntosDifusosTest extends AnyFunSuite {
 
   test("Test de pertenencia - Un elemento pertenece a un conjunto difuso") {
     val conjunto = conjDifuso.grande(5)
-    assert(conjDifuso.pertenece(4, conjunto) === 0.8)  // 4 / 5 = 0.8
-    assert(conjDifuso.pertenece(5, conjunto) === 1.0)  // Igual a d, devuelve 1.0
-    assert(conjDifuso.pertenece(10, conjunto) === 1.0) // Igual a d, devuelve 1.0
+    assert(conjDifuso.pertenece(4, conjunto) === 0.8)
+    assert(conjDifuso.pertenece(5, conjunto) === 1.0)
+    assert(conjDifuso.pertenece(10, conjunto) === 1.0)
   }
 
   test("Test de pertenencia - Elemento fuera del rango") {
     val conjunto = conjDifuso.grande(5)
-    assert(conjDifuso.pertenece(3, conjunto) === 0.6)  // 3 / 5 = 0.6
-    assert(conjDifuso.pertenece(6, conjunto) === 1.0)  // Igual a d, devuelve 1.0
+    assert(conjDifuso.pertenece(3, conjunto) === 0.6)
+    assert(conjDifuso.pertenece(6, conjunto) === 1.0)
   }
+
+  test("Test de grande - Elemento menor que d") {
+    val conjunto = conjDifuso.grande(5)
+    assert(conjDifuso.pertenece(3, conjunto) === 0.6)
+    assert(conjDifuso.pertenece(2, conjunto) === 0.4)
+    assert(conjDifuso.pertenece(1, conjunto) === 0.2)
+  }
+
+  test("Test de grande - Elemento mayor o igual a d") {
+    val conjunto = conjDifuso.grande(5)
+    assert(conjDifuso.pertenece(5, conjunto) === 1.0)
+    assert(conjDifuso.pertenece(6, conjunto) === 1.0)
+  }
+
 
   test("Test de igualdad - Conjuntos con rangos iguales") {
     val conjunto1 = conjDifuso.grande(5)
     val conjunto2 = conjDifuso.grande(5)
-    assert(conjDifuso.igualdad(conjunto1, conjunto2, 1 to 5)) // Conjuntos iguales con el mismo rango
+    assert(conjDifuso.igualdad(conjunto1, conjunto2, 1 to 5))
   }
 
   test("Test de igualdad - Conjuntos diferentes") {
     val conjunto1 = conjDifuso.grande(5)
     val conjunto2 = conjDifuso.grande(4)
-    assert(!conjDifuso.igualdad(conjunto1, conjunto2, 1 to 10)) // Diferentes conjuntos
+    assert(!conjDifuso.igualdad(conjunto1, conjunto2, 1 to 10))
+  }
+
+  test("Test de unión - Unión de dos conjuntos con el mismo valor para un elemento") {
+    val conjunto1 = conjDifuso.grande(5)
+    val conjunto2 = conjDifuso.grande(5)
+    val unionConjunto = conjDifuso.union(conjunto1, conjunto2)
+    assert(conjDifuso.pertenece(4, unionConjunto) === 0.8)
+  }
+
+  test("Test de unión - Unión de dos conjuntos con diferentes valores para un elemento") {
+    val conjunto1 = conjDifuso.grande(5)
+    val conjunto2 = conjDifuso.grande(3)
+    val unionConjunto = conjDifuso.union(conjunto1, conjunto2)
+    assert(conjDifuso.pertenece(4, unionConjunto) === 1.0)
+
+  }
+
+  test("Test de intersección de conjuntos difusos") {
+    val conjunto1 = conjDifuso.grande(5)
+    val conjunto2 = conjDifuso.grande(3)
+    val interseccionConjunto = conjDifuso.intersect(conjunto1, conjunto2, 1 to 5)
+    assert(conjDifuso.pertenece(3, interseccionConjunto) === 0.6)
+    assert(conjDifuso.pertenece(5, interseccionConjunto) === 1.0)
+    assert(conjDifuso.pertenece(6, interseccionConjunto) === 0.0)
   }
 
 }
